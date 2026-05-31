cd2fc58ef8c5f6ff6f07539e1f1988ea2e5ba28d
KFC_PAJINKO <saharat.windows@gmail.com>
2026-05-31 13:21:58 +0700

    change in connect.php

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
 $mysql = new mysqli('localhost','root','root','guitarshop',3306);
 if($mysql->connect_errno)
 {
-    echo $mysqli -> connect_error.": ".$mysqli->connect_error;
+    echo $mysql -> connect_error.": ".$mysql->connect_error;
 }
 ?>
